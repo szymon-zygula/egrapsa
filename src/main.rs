ebdbb0cb3fe967e96cbaa8e03b4b508adcc71ca2
Szymon Zyguła <szymon@zygula.eu>
2024-03-16 21:30:30 +0100

    Add some CLI

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(version, about)]
+struct Cli {
+    name: Option<String>,
+
+    #[command(subcommand)]
+    command: Option<Subcommands>
+}
+
+#[derive(Subcommand)]
+enum Subcommands {
+    Book {
+        #[arg(short, long)]
+        text_source: String
+    },
+    OperaOmnia {
+        #[arg(short, long)]
+        author_source: String
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
 }
